8eb5be343daa0618994261fe3792c0ed37d5807d
Timmu Ründal <timukasr@gmail.com>
2015-01-21 02:10:29 +0200

    Simple UI for drag-dropping files.

--- components/pages/UpdateTransaction.jsx
+++ components/pages/UpdateTransaction.jsx
@@ -19,8 +19,21 @@ var Transactions = React.createClass({
       amount: '-15.45',
       method: 'credit',
       errors: {},
-      hasErrors: false
+      hasErrors: false,
+      dragActive: false,
+      dragInZone: false,
+      files: [],
+      dragFileName: 'choose file'
     };
+  },
+  componentDidMount: function() {
+    document.addEventListener('dragover', this._onDragStart);
+    document.addEventListener('dragleave', this._onDragEnd);
+  },
+
+  componentWillUnmount: function() {
+    document.removeEventListener('dragover', this._onDragStart);
+    document.removeEventListener('dragleave', this._onDragEnd);
   },
   render: function() {
     var errorMessage = null;
@@ -34,6 +47,13 @@ var Transactions = React.createClass({
       );
     }
 
+    var dropZoneStyle = {
+      borderWidth: '1px',
+      borderColor: '#666',
+      borderStyle: this.state.dragInZone ? 'solid' : 'dashed',
+      backgroundColor: this.state.dragActive ? 'yellow' : 'cyan'
+    };
+
     return (
       <div>
         <h2>Create transaction</h2>
@@ -42,7 +62,8 @@ var Transactions = React.createClass({
           <Input type="datetime-local" label="Date" labelClassName="col-xs-2" wrapperClassName="col-xs-10"
             valueLink={this.linkState('date')} help={errors.date} bsStyle={errors.date ? 'error' : null} />
           <Input type="textarea" label="Description" labelClassName="col-xs-2" wrapperClassName="col-xs-10"
-            valueLink={this.linkState('description')} help={errors.description} bsStyle={errors.description ? 'error' : null} />
+            valueLink={this.linkState('description')}
+            help={errors.description} bsStyle={errors.description ? 'error' : null} />
           <Input type="text" label="Location" labelClassName="col-xs-2" wrapperClassName="col-xs-10"
             valueLink={this.linkState('location')} help={errors.location} bsStyle={errors.location ? 'error' : null} />
           <Input type="number" label="Amount" labelClassName="col-xs-2" wrapperClassName="col-xs-10"
@@ -56,10 +77,30 @@ var Transactions = React.createClass({
             <option value="bank">Bank</option>
             <option value="cash">Cash</option>
           </Input>
+          <div className="col-xs-offset-2 col-xs-10" style={dropZoneStyle}
+            onDragStart={this._onDragStart} onDragEnd={this._onDragEnd}
+            onDragLeave={this._onDragLeave} onDragOver={this._onDragOver} onDrop={this._onDrop}
+          >
+            {this.state.files.map(function(file) {
+              return (<div onClick={this._onRemoveFile.bind(this, file)}>{file.name}</div>);
+            }.bind(this))}
+            {this.state.dragFileName}
+          </div>
           <Input type="submit" value="Save" wrapperClassName="col-xs-offset-2 col-xs-10" />
         </form>
       </div>
     );
+  },
+  _onRemoveFile: function(file) {
+    var index = this.state.files.indexOf(file);
+
+    if (index !== -1) {
+      this.state.files.splice(index, 1);
+
+      this.setState({
+        files: this.state.files
+      });
+    }
   },
   _onSubmit: function(e) {
     e.preventDefault();
@@ -81,6 +122,54 @@ var Transactions = React.createClass({
     });
 
     this.props.context.executeAction(createTransaction, result.data);
+  },
+  _onDragStart: function(e) {
+    e.preventDefault();
+
+    if (!this.state.dragActive) {
+      this.setState({
+        dragActive: true
+      });
+    }
+
+    e.dataTransfer.dropEffect = this.state.dragInZone ? 'copy' : 'none';
+  },
+  _onDragEnd: function() {
+    this.setState({
+      dragActive: false
+    });
+  },
+  _onDragOver: function(e) {
+    e.preventDefault();
+
+    if (!this.state.dragInZone) {
+      this.setState({
+        dragInZone: true
+      });
+    }
+  },
+  _onDragLeave: function() {
+    this.setState({
+      dragInZone: false,
+      dragFileName: 'choose file'
+    });
+  },
+  _onDrop: function(e) {
+    console.log('drop', e.target);
+    e.preventDefault();
+    //e.stopPropagation();
+
+    var file = e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files[0];
+
+    if (file) {
+      this.state.files.push(file);
+    }
+
+    this.setState({
+      dragInZone: false,
+      dragActive: false,
+      files: this.state.files
+    });
   }
 });
 
