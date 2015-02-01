59122236a42dd55b0407363c8740770a3d293d19
Timmu Ründal <timukasr@gmail.com>
2015-02-01 20:02:54 +0200

    Show thumbnails in FileGallery

--- components/FileGallery.jsx
+++ components/FileGallery.jsx
@@ -3,6 +3,9 @@
 var React = require('react/addons');
 var CreateTransactionStore = require('../stores/CreateTransactionStore');
 var StoreMixin = require('fluxible').StoreMixin;
+var ReactBootstrap = require('react-bootstrap');
+var Glyphicon = ReactBootstrap.Glyphicon;
+var cx = React.addons.classSet;
 
 var FileGallery = React.createClass({
   mixins: [StoreMixin],
@@ -21,13 +24,35 @@ var FileGallery = React.createClass({
   },
   render: function() {
     return (
-      <div>
-        {this.state.files.map(this._renderFile)}
-      </div>
-    )
+      this.state.files.length === 0
+        ? null :
+        <div className="fileList">
+          {this.state.files.map(this._renderFile)}
+        </div>
+    );
   },
-  _renderFile: function(file, index) {
-    return <p key={file.id}>{index + 1}. {file.title}</p>;
+  _renderFile: function(file) {
+    var hasThumbnail = !!file.thumbnailLink;
+    var viewUrl = file.embedLink || file.alternateLink; // TODO use webcontentLink and img tag
+    var classes = cx({
+      file: true,
+      placeholder: !hasThumbnail
+    });
+
+    var fileElement = hasThumbnail ? <img src={file.thumbnailLink} title={file.title} /> : file.title;
+
+    return (
+      <div key={file.id} className={classes}>
+        <a href={viewUrl} target="_blank">
+          {fileElement}
+          <Glyphicon glyph="remove" className="text-danger" onClick={this._onRemoveFile.bind(this, file)} />
+        </a>
+      </div>
+    );
+  },
+  _onRemoveFile: function(file, event) {
+    event.preventDefault();
+    // TODO implement
   }
 });
 
