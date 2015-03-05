e725374032f99304c30acb5d7383a30dba6057c7
Timmu Ründal <timukasr@gmail.com>
2015-03-05 23:39:24 +0200

    Allow reloading server without down-time (npm run dev).

--- dev-server.js
+++ dev-server.js
@@ -1,19 +1,58 @@
 'use strict';
 
-var webpack          = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var webpackConfig    = require('./webpack.config');
+var cluster = require('cluster');
 
-// Run the webpack dev server
-var webpackServer = new WebpackDevServer(webpack(webpackConfig), {
+if (!cluster.isMaster) {
+  return require('./server');
+}
+
+console.log('started master with ' + process.pid);
+
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('./webpack.config');
+var webpack = require('webpack');
+var watch = require('watch');
+
+// start child process that is a actual server
+cluster.fork();
+
+// start webpack
+var server = new WebpackDevServer(webpack(webpackConfig), {
   publicPath: webpackConfig.output.publicPath,
   contentBase: 'http://localhost:3005',
-  noInfo: true,
   hot: true,
+  noInfo: true,
+  quiet: true,
   headers: {
     "Access-Control-Allow-Origin": "*"
   }
-}).listen(3006, 'localhost', function (err, result) {
-    if (err) console.log(err);
-    console.log('bueno')
+});
+
+server.listen(3006, 'localhost', function() {
+  console.log('Webpack server listening on port 3006');
+});
+
+// start file watcher for server reload
+watch.watchTree('.', function (file) {
+  if (typeof file === 'string' && (file.slice(-3) === '.js' || file.slice(-4) === '.jsx')) {
+    reloadWorker();
+  }
+});
+
+function reloadWorker() {
+  console.log('starting new worker');
+
+  var newWorker = cluster.fork();
+
+  newWorker.once('listening', function () {
+    console.log('new worker ready');
+    //stop all other workers
+    for (var id in cluster.workers) {
+      if (id === newWorker.id.toString()) {
+        continue;
+      }
+
+      cluster.workers[id].process.kill('SIGTERM');
+    }
   });
+}
